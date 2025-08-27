d65bd20c9c97110d352b9494029453c37f3569e1
rachitha <2022cs154@stu.ucsc.cmb.ac.lk>
2025-08-27 20:22:13 +0530

    chat app started

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:chatapp/pages/signup_page.dart';
+import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+import 'pages/Home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Chat App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: "/login",
+      routes: {
+        "/login": (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
+        "/signup": (context) => const SignUpPage(),
+      },
+    );
+  }
+}
